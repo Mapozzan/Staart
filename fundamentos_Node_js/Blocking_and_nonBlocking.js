83cc1827a41864e77dde8b9e2ed3378f5fc9bef1
marco <mpozzan93@gmail.com>
2022-07-03 16:44:19 -0300

    Continuando codigos de aprendizado

---
+++ fundamentos_Node_js/Blocking_and_nonBlocking.js
@@ -0,0 +1,56 @@
+const {
+    readFileSync,
+    writeFileSync,
+    readFile,            //Asynchronously reads the entire contents of a file.
+    writeFile,           //When file is a filename, asynchronously writes data to the file, replacing the file if it already exists. data can be a string or a buffer
+} = require('fs')
+
+const {
+    join,
+} = require('path')  //Encontrar o caminho do arquivo, independente do OS
+
+
+const logDuration = (label,startTime) => {
+    console.log(`${label} levou ${Date.now - startTime} ms`)
+}
+
+const copyFileBlocking = (source, dest) => {
+    const startTime = Date.now()
+    
+    console.log('Lendo blocking content')
+    const content = readFileSync(source)
+
+    console.log('Escrevendo blocking content')
+    writeFileSync(dest, content)
+
+    logDuration('copyFileBlocking',startTime)
+}
+
+const sourcePath = join(__dirname ,'files', 'example.txt')
+const destPath = join(__dirname ,'files', 'example.copy.blocking.txt')
+
+copyFileBlocking(sourcePath,destPath)
+console.log('*'.repeat(30))
+
+const copyFileNonBlocking = (source, dest) => {
+    const startTime = Date.now()
+    console.log('Começou a copia non-blocking')
+
+    readFile(source, (_err, data) => {  
+        console.log('Terminou de Ler non-blocking')
+        writeFile(dest,data, (_err) => {
+            
+            console.log('Terminou de escrever non-blocking')
+
+            logDuration('copyFileNonBlocking',startTime)
+        })
+    })
+}
+
+
+const destPathNonBlocking = join(__dirname ,'files', 'example.copy.nonblocking.txt')
+
+copyFileNonBlocking(sourcePath, destPathNonBlocking)
+
+console.log('Terminou mesmo?')
+console.log('Continuando')
